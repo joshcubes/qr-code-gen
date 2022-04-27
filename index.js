43ad99cff776cd29b82950da17bf47391e2e0bf5
joshcubes <76458322+joshcubes@users.noreply.github.com>
2022-04-27 21:16:39 +0100

    Final Commit

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var QRCode = require('qrcode')
+const prompt = require('prompt');
 
-QRCode.toString('I am a pony!',{type:'terminal'}, function (err, code) {
-  if (err) throw err
-  console.log(code)
-})
+var input = ""
+
+prompt.start();
+
+prompt.get(['input'], function (err, result) {
+  if (err) {
+    return onErr(err);
+  }
+  QRCode.toString(result.input ,{type:'terminal'}, function (err, code) {
+    if (err) throw err
+    console.log(code)
+  })
+});
+
+function onErr(err) {
+  console.log(err);
+  return 1;
+}
+
