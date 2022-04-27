06ca9161c9453b635c2582df5ae5d98fcf544cba
joshcubes <76458322+joshcubes@users.noreply.github.com>
2022-04-27 21:11:20 +0100

    Change Prompts

--- prompt.js
+++ prompt.js
@@ -2,13 +2,11 @@ const prompt = require('prompt');
 
 prompt.start();
 
-prompt.get(['username', 'email'], function (err, result) {
+prompt.get(['input'], function (err, result) {
   if (err) {
     return onErr(err);
   }
-  console.log('Command-line input received:');
-  console.log('  Username: ' + result.username);
-  console.log('  Email: ' + result.email);
+  const input = result.input;
 });
 
 function onErr(err) {
